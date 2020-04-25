23e64d9a26c6c32108993718b4e36b088bc10c7a
ajaythompson_tdt <ajay.thompson@thedatateam.in>
2020-04-25 16:22:10 +0530

    added model kafka object

---
+++ src/main/scala/org/thompson/ingestion/model/KafkaObject.scala
@@ -0,0 +1,9 @@
+package org.thompson.model
+
+import org.json4s.Formats
+import org.json4s.jackson.Serialization.write
+
+case class KafkaObject[T <: ModelWithKey](key: String, value: String) {
+
+  def this(t: T)(implicit format: Formats) = this(t.getKey, write(t))
+}
