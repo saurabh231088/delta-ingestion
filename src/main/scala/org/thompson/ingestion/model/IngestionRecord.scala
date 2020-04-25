42b16e86a61c3ebf1d592af0297e0e34a3ef496e
ajaythompson_tdt <ajay.thompson@thedatateam.in>
2020-04-25 16:23:11 +0530

    added ingestion record mandatory propeties

--- src/main/scala/org/thompson/ingestion/model/IngestionRecord.scala
+++ src/main/scala/org/thompson/ingestion/model/IngestionRecord.scala
@@ -11,3 +11,14 @@ import java.sql.Timestamp
  * @tparam T payload class.
  */
 case class IngestionRecord[T](recordInfo: Map[String, String], payLoad: T, insertTime: Timestamp)
+    extends ModelWithKey {
+
+  val SOURCE_NAME_KEY = "source_name"
+  val TABLE_NAME_KEY = "table_name"
+
+  // check all required info are available
+  val requiredRecordInfo = List(SOURCE_NAME_KEY, TABLE_NAME_KEY)
+  require(requiredRecordInfo.forall(recordInfo.keys.toList.contains(_)))
+
+  def getKey: String = s"${SOURCE_NAME_KEY}.${TABLE_NAME_KEY}"
+}
