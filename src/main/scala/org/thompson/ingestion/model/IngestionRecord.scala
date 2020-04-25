fe769c429ad48bba68210919d2491fda4cad8aad
ajaythompson_tdt <ajay.thompson@thedatateam.in>
2020-04-25 08:18:57 +0530

    code formatting

--- src/main/scala/org/thompson/ingestion/model/IngestionRecord.scala
+++ src/main/scala/org/thompson/ingestion/model/IngestionRecord.scala
@@ -3,12 +3,11 @@ package org.thompson.ingestion.model
 import java.sql.Timestamp
 
 /**
+ * Ingestion record structure.
  *
- * @param recordInfo
- * @param payLoad
- * @param insertTime
- * @tparam T
+ * @param recordInfo Information about the record.
+ * @param payLoad    The record to be ingested.
+ * @param insertTime The time when the record was inserted to queue.
+ * @tparam T payload class.
  */
-case class IngestionRecord[T](recordInfo: Map[String, String],
-                              payLoad: T,
-                              insertTime: Timestamp)
+case class IngestionRecord[T](recordInfo: Map[String, String], payLoad: T, insertTime: Timestamp)
