c8cfaad4c7e471c61a4f851740dde585eb61a902
ajaythompson_tdt <ajay.thompson@thedatateam.in>
2020-04-29 18:50:08 +0530

    added variable for magic strings

--- src/main/scala/org/thompson/ingestion/BatchIngestUtil.scala
+++ src/main/scala/org/thompson/ingestion/BatchIngestUtil.scala
@@ -3,15 +3,22 @@ package org.thompson.ingestion
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
 object BatchIngestUtil {
+
+  val RECORD_INFO_KEY = "recordInfo"
+  val PAYLOAD_INFO_KEY = "payload"
+  val TABLE_NAME_KEY = "table_name"
+  val EVENT_VALUE_KEY = "value"
+
   def batchIngest(dataFrame: DataFrame)(implicit spark: SparkSession): Array[(Row, DataFrame)] = {
     import spark.implicits._
-    val df: DataFrame = spark.read.json(dataFrame.map(_.get(1).toString))
-    val tables = df.select($"recordInfo.table_name").distinct().collect().map(_.getString(0))
+    val df: DataFrame = spark.read.json(dataFrame.map(_.getAs[String](EVENT_VALUE_KEY)))
+    val tables = df.select($"${RECORD_INFO_KEY}.${TABLE_NAME_KEY}")
+      .distinct().collect().map(_.getAs[String](TABLE_NAME_KEY))
     tables.map(tableName => {
-      val tableDF = df.filter($"recordInfo".getField("table_name") === tableName)
+      val tableDF = df.filter($"recordInfo".getField(TABLE_NAME_KEY) === tableName)
       (
-        tableDF.select($"recordInfo.*").head,
-        tableDF.select($"payload.*"))
+        tableDF.select($"${RECORD_INFO_KEY}.*").head,
+        tableDF.select($"${PAYLOAD_INFO_KEY}.*"))
     })
   }
 }
