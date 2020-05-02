8f93f9417a5a15c26d134a149d68ce38ce0b22f7
ajaythompson_tdt <ajay.thompson@thedatateam.in>
2020-05-02 19:51:13 +0530

    added updated employee schema

--- src/test/scala/org/thompson/ingestion/model/Employee.scala
+++ src/test/scala/org/thompson/ingestion/model/Employee.scala
@@ -1,3 +1,5 @@
 package org.thompson.ingestion.model
 
 final case class Employee(id: Int, name: String, department: String)
+
+final case class NewEmployee(id: Int, name: String, department: String, mobile: String)
